66303ab705f723decdaf9e60ea207ab32a0eb9b5
giovannimhern <giovannimhern@uchicago.edu>
2024-07-23 15:44:09 -0700

    imports & modules

---
+++ go.mod
@@ -0,0 +1,3 @@
+module aws-go
+
+go 1.22.0
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"aws-go/imports" // Adjust the import path based on your project structure
 	"fmt"
 )
 
 func main(){
-	fmt.Println(("hello, world!"))
+	newTicket := imports.Ticket{
+		ID: 123, 
+		Event: "Course walkthrough", 
+	}
+	fmt.Println(newTicket)
 }
